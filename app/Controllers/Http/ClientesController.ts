c45f63bb4238caacce478089b101d1c7f05eeb04
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-06-06 11:31:00 -0400

    docs: doc atualizado

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -4,44 +4,9 @@ import ClienteService from "App/Services/ClienteService";
 export default class ClientesController {
   /**
    * @swagger
-   * tags:
-   *   name: Users
-   *   description: Operações relacionadas a usuários
-   */
-
-  /**
-   * @swagger
-   * /users:
-   *   get:
-   *     summary: Retorna a lista de usuários
-   *     tags: [Users]
-   *     responses:
-   *       200:
-   *         description: Lista de usuários
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: array
-   *               items:
-   *                 $ref: '#/components/schemas/User'
-   */
-
-  /**
-   * @swagger
-   * components:
-   *   schemas:
-   *     User:
-   *       type: object
-   *       required:
-   *         - id
-   *         - name
-   *       properties:
-   *         id:
-   *           type: integer
-   *           description: O ID do usuário
-   *         name:
-   *           type: string
-   *           description: O nome do usuário
+   * /clientes:  
+   *   get:  
+   *     summary: Retorna a lista de clientes
    */
   public async store({ request }: HttpContextContract) {
     try {
--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -6,7 +6,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Minha API AdonisJS",
+      title: "Documentation API",
       version: "1.0.0",
     },
   },
